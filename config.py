113f157a055ed519ace2ca14abebf56a3c2e84a3
Noam Shaked Cohen <noamshakedc@c-008.cs.tau.ac.il>
2025-04-26 14:31:18 +0300

    training simple models on the allot ds

--- config.py
+++ config.py
@@ -2,10 +2,126 @@ from dataclasses import dataclass, field
 from pathlib import Path
 import torch
 
+'''
+{'Samsung Galaxy Store': 0,
+ 'WhatsApp Transfer': 1,
+ 'Xiaomi Services': 2,
+ 'Vungle': 3,
+ 'HTTP': 4,
+ 'FaceTime': 5,
+ 'Google RCS': 6,
+ 'NTP': 7,
+ 'Xiaomi Cloud': 8,
+ 'Skype': 9,
+ 'Temu': 10,
+ 'iTunes': 11,
+ 'WeChat': 12,
+ 'BitTorrent': 13,
+ 'Bybit': 14,
+ 'Instagram': 15,
+ 'TLS with Encrypted Client Hello': 16,
+ 'DNS over HTTPS': 17,
+ 'DNS over TLS': 18,
+ 'Google Search': 19,
+ 'Office 365': 20,
+ 'GoogleHangout': 21,
+ 'Waze': 22,
+ 'Akamai': 23,
+ 'Quic Obfuscated': 24,
+ 'Snapchat': 25,
+ 'YouTube': 26,
+ 'SamsungServices': 27,
+ 'Roblox': 28,
+ 'TCP Port 443': 29,
+ 'STUN': 30,
+ 'Telegram': 31,
+ 'Facebook': 32,
+ 'Facebook Calls': 33,
+ 'BitTorrent Tracker': 34,
+ 'FacebookContent': 35,
+ 'NetFlix_Browsing': 36,
+ 'Office 365 OutLook': 37,
+ 'MS OutLook': 38,
+ 'Samsung Cloud': 39,
+ 'HTTP_Browsing': 40,
+ 'BitTorrentDHT': 41,
+ 'NetFlix': 42,
+ 'Gmail': 43,
+ 'Twitter': 44,
+ 'Imo': 45,
+ 'Amazon Services': 46,
+ 'Liftoff': 47,
+ 'WindowsUpdate': 48,
+ 'Facebook Video': 49,
+ 'Spotify': 50,
+ 'Truecaller': 51,
+ 'Mobile Apple Store': 52,
+ 'MS Services': 53,
+ 'Crashlytics': 54,
+ 'Google Push Notifications': 55,
+ 'byteoversea': 56,
+ 'TLS with no SNI': 57,
+ 'Viber': 58,
+ 'WhatsApp': 59,
+ 'Yahoo Management': 60,
+ 'Google Calendar': 61,
+ 'Game sites': 62,
+ 'Google Maps': 63,
+ 'Online radio and online TV sites': 64,
+ 'Pay per surf sites': 65,
+ 'AmazonCloud': 66,
+ 'iCloud': 67,
+ 'Facebook Chat': 68,
+ 'Apple Push Notification': 69,
+ 'Supercell': 70,
+ 'GoogleServices': 71,
+ 'Linkedin': 72,
+ 'Mobile phone sites': 73,
+ 'Shopping sites': 74,
+ 'Travel sites': 75,
+ 'Ynet': 76,
+ 'Business sites': 77,
+ 'Press sites': 78,
+ 'Computing sites': 79,
+ 'Alibaba': 80,
+ 'Banners': 81,
+ 'AliExpress': 82,
+ 'Search engine sites': 83,
+ 'yad2': 84,
+ 'Pinterest': 85,
+ 'Google Play': 86,
+ 'Google UserContent': 87,
+ '365Scores': 88,
+ 'Directory and street map sites': 89,
+ 'PUBG': 90,
+ 'YouTube Browsing': 91,
+ 'Advertisements': 92,
+ 'Generic CDN': 93,
+ 'HTTPS_Streaming': 94,
+ 'Analytics': 95,
+ 'iCloud Private Relay': 96,
+ 'TikTok Live': 97,
+ 'Unity 3D': 98,
+ 'Wolt': 99,
+ 'Moovit': 100,
+ 'Google Cloud': 101,
+ 'Google Meet': 102,
+ 'HTTPS': 103,
+ 'Tik Tok': 104,
+ 'Siri': 105,
+ 'Weather.com': 106,
+ 'AppleServices': 107,
+ 'AppleCalendar': 108,
+ 'Cloudflare': 109,
+ 'WhatsApp Calls': 110,
+ 'Yandex': 111,
+ 'Adult Streaming': 112}
+'''
+
 @dataclass
 class Config:
     # Experiment Details
-    EXPERIMENT_NAME: str = "deleteme"
+    EXPERIMENT_NAME: str = "allot_simple_v3_log"
     EXPERIMENT_PATH: Path = field(init=False)
 
     # Environment and Reproducibility
@@ -30,11 +146,12 @@ class Config:
     # Model Parameters
     MODEL_PARAMS: dict = field(default_factory=lambda: {
         'conv_type': '1d',
-        'input_shape': 256,
+        # 'input_shape': 256, # This will be set in __post_init__
         'conv_layers': [
-            {'out_channels': 4, 'kernel_size': 3, 'stride': 1, 'padding': 1},
-            {'out_channels': 8, 'kernel_size': 3, 'stride': 1, 'padding': 1},
-            # {'out_channels': 64, 'kernel_size': 3, 'stride': 1, 'padding': 1}
+            {'out_channels': 16, 'kernel_size': 3, 'stride': 1, 'padding': 1},
+            {'out_channels': 32, 'kernel_size': 3, 'stride': 1, 'padding': 1},
+            {'out_channels': 64, 'kernel_size': 3, 'stride': 1, 'padding': 1},
+            {'out_channels': 64, 'kernel_size': 3, 'stride': 1, 'padding': 1},
         ],
         'pool_kernel_size': 2,
         'pool_stride': 2,
@@ -49,15 +166,15 @@ class Config:
     })
 
     # Training Parameters
-    LEARNING_RATE: float = 3e-4
-    NUM_EPOCHS: int = 20
+    LEARNING_RATE: float = 3e-3
+    NUM_EPOCHS: int = 100
     WEIGHT_DECAY: float = 1e-4
 
     # Domain Adaptation Parameters
     LAMBDA_MMD: float = 0
     MMD_BANDWIDTHS: list = field(default_factory=lambda: [1e-1, 1e0, 1e1])
     
-    LAMBDA_DANN: float = 1e0
+    LAMBDA_DANN: float = 0 # 1e0
 
     # Logging and Checkpoints
     SAVE_PLOTS: bool = True
@@ -70,4 +187,4 @@ class Config:
         self.EXPERIMENT_PLOTS_PATH = self.EXPERIMENT_PATH / "plots"
         self.EXPERIMENT_WEIGHTS_PATH = self.EXPERIMENT_PATH / "weights"
         self.SAVE_MODEL_CHECKPOINT = self.EXPERIMENT_WEIGHTS_PATH / "model_checkpoint_epoch_{epoch}.pth"
-        self.MODEL_PARAMS['input_size'] = self.RESOLUTION
+        self.MODEL_PARAMS['input_shape'] = self.RESOLUTION
--- data_utils/csv_dataloader.py
+++ data_utils/csv_dataloader.py
@@ -1,4 +1,6 @@
 import re
+import warnings
+
 
 import numpy as np
 import pandas as pd
@@ -7,17 +9,32 @@ from torch.utils.data import Dataset, DataLoader
 
 MTU = 1500  # Maximum Transmission Unit in bytes
 
-def session_2d_histogram(ts, sizes, resolution=MTU, max_delta_time=10):
-    ts_norm = ((np.array(ts) - ts[0]) / max_delta_time) * MTU
-    bin_edges = np.linspace(0, MTU, resolution + 1)
-    H, _, _ = np.histogram2d(sizes, ts_norm, bins=(bin_edges, bin_edges))
+def session_2d_histogram(ts, sizes, resolution=MTU, max_delta_time=10, log_t_axis=False):
+    ts_array = np.array(ts)
+    sizes_array = np.array(sizes)
+    
+    # Normalize timestamps
+    ts_norm = ((ts_array - ts_array[0]) / max_delta_time) * MTU
+    
+    if log_t_axis:
+        ts_norm = np.log1p(ts_norm)  # log(1 + x)
+        log_max_time = np.log1p(MTU)
+        bin_edges_x = np.linspace(0, log_max_time, resolution + 1)
+    else:
+        bin_edges_x = np.linspace(0, MTU, resolution + 1)
+    
+    bin_edges_y = np.linspace(0, MTU, resolution + 1)
+
+    H, _, _ = np.histogram2d(sizes_array, ts_norm, bins=(bin_edges_y, bin_edges_x))
     return H.astype(np.uint16)
     
 class CSVFlowPicDataset(Dataset):
-    def __init__(self, csv_paths, resolution=MTU, max_dt_ms=30000):
+    def __init__(self, csv_paths, resolution=MTU, max_dt_ms=30000, label_mapping=None, log_t_axis=False): 
         self.csv_paths = csv_paths
         self.resolution = resolution
         self.max_delta_time = max_dt_ms
+        self.label_mapping = label_mapping
+        self.log_t_axis = log_t_axis
         
         # Index all sessions at initialization for efficiency
         self.sessions = []
@@ -36,8 +53,16 @@ class CSVFlowPicDataset(Dataset):
             for _, row in df.iterrows():
                 ts = row['ppi-pdt']
                 if len(ts) >= 1:
+                    if self.label_mapping is not None:
+                        app_id = row['appId']
+                        if app_id in self.label_mapping:
+                            self.labels.append(self.label_mapping[app_id])
+                        else:
+                            warnings.warn(f"App ID {app_id} not found in label mapping.")
+                            continue
+                    else:     
+                        self.labels.append(row['appId'])
                     self.sessions.append((ts, row['ppi-ps']))
-                    self.labels.append(row['appId'])
                     self.rows.append(row)
 
     def __len__(self):
@@ -45,9 +70,9 @@ class CSVFlowPicDataset(Dataset):
 
     def __getitem__(self, idx):
         ts, sizes = self.sessions[idx]
-        flowpic = session_2d_histogram(ts, sizes, self.resolution, self.max_delta_time)
+        flowpic = session_2d_histogram(ts, sizes, self.resolution, self.max_delta_time, log_t_axis=self.log_t_axis)
 
-        flowpic_tensor = torch.tensor(flowpic, dtype=torch.float32).unsqueeze(0)
+        flowpic_tensor = torch.tensor(flowpic, dtype=torch.float32) # .unsqueeze(0)
         label_tensor = torch.tensor(self.labels[idx], dtype=torch.long)
         
         # can get other features from self.rows[idx] if needed
@@ -55,7 +80,9 @@ class CSVFlowPicDataset(Dataset):
         return flowpic_tensor, label_tensor
 
 
-def create_csv_flowpic_loader(csv_paths, batch_size=64, shuffle=True, num_workers=4, resolution=MTU, max_dt_ms=30000):
-    dataset = CSVFlowPicDataset(csv_paths, resolution=resolution, max_dt_ms=max_dt_ms)
+def create_csv_flowpic_loader(csv_paths, batch_size=64, shuffle=True, num_workers=4,
+                              resolution=MTU, max_dt_ms=30000, label_mapping=None, log_t_axis=False):
+    dataset = CSVFlowPicDataset(csv_paths, resolution=resolution, max_dt_ms=max_dt_ms,
+                                label_mapping=label_mapping, log_t_axis=log_t_axis)
     loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
     return loader
